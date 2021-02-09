e6380894a2363391d27948fdd549536d06564ef1
Brendhon <brendhon.e.c.m@gmail.com>
2021-02-09 11:12:45 -0300

    implement: Lazy loading

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AboutComponent } from "./about.component";
+import { RouterModule, Routes } from '@angular/router';
+
+// Declarando as Rotas deste Môdulo
+const ROUTES: Routes = [
+  {path: '', component: AboutComponent}
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterModule.forChild(ROUTES), // Utilizando "forChild" por causa de ser uma rota filha
+  ],
+  declarations: [
+    AboutComponent,
+  ],
+})
+export class AboutModule {}
