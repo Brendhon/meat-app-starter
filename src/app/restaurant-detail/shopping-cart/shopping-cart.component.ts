6d4e43349b40e7acd3c71ad976ae88f3ca874df8
Brendhon <brendhon.e.c.m@gmail.com>
2021-02-10 11:21:03 -0300

    implement: keyframes

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,11 +1,44 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  animate,
+  keyframes,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+
 import { ShoppingCartService } from "./shopping-cart.service";
 
 @Component({
   selector: "mt-shopping-cart",
   templateUrl: "./shopping-cart.component.html",
+  animations: [
+    trigger("row", [
+      state("ready", style({ opacity: 1 })),
+      transition(
+        "void => ready",
+        animate("300ms 0s ease-in",
+        keyframes([
+          style({ opacity: 0, transform: "translateX(-30px)", offset: 0 }),
+          style({ opacity: 0.8, transform: "translateX(10px)", offset: 0.8 }),
+          style({ opacity: 1, transform: "translateX(0px)", offset: 1 }),
+        ]))),
+      transition(
+        "ready => void",
+        animate("300ms 0s ease-in",
+        keyframes([
+          style({ opacity: 1, transform: "translateX(0px)", offset: 0 }),
+          style({ opacity: 0.2, transform: "translateX(-10px)", offset: 0.2 }),
+          style({ opacity: 0, transform: "translateX(30px)", offset: 1 }),
+        ]))),
+    ]),
+  ],
 })
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready'
+
   constructor(private shoppingCartService: ShoppingCartService) {}
 
   ngOnInit() {
@@ -20,14 +53,14 @@ export class ShoppingCartComponent implements OnInit {
   }
 
   clear() {
-    this.shoppingCartService.clear()
+    this.shoppingCartService.clear();
   }
 
   removeItem(item: any) {
-    this.shoppingCartService.removeItem(item)
+    this.shoppingCartService.removeItem(item);
   }
 
   addItem(item: any) {
-    this.shoppingCartService.addItem(item)
+    this.shoppingCartService.addItem(item);
   }
 }
