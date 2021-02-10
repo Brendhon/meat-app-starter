102e554282b38e6bb29ad69e1c0d30f340bbaeca
Brendhon <brendhon.e.c.m@gmail.com>
2021-02-10 10:56:06 -0300

    implement: animated restaurant

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,13 +1,32 @@
 import { Component, Input, OnInit } from "@angular/core";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 import { Restaurant } from "./restaurant.model";
 
 @Component({
   selector: "mt-restaurant",
   templateUrl: "./restaurant.component.html",
+  animations: [
+    trigger("restaurantAppeared", [
+      state("ready", style({ opacity: 1 })),
+      // Void => Ready - Do momento em que ele é adicionado a arvore de elementos para o state ready
+      transition("void => ready", [
+        style({ opacity: 0, transform: "translate(50px)" }),
+        animate("500ms 0s ease-in-out"),
+      ]),
+    ]),
+  ],
 })
 export class RestaurantComponent implements OnInit {
   @Input()
-  restaurant: Restaurant; 
+  restaurant: Restaurant;
+
+  restaurantState = 'ready'
 
   constructor() {}
 
