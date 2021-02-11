2ee24112530651a94a84fe5e3e7e09b6b5e8a5a4
Brendhon <brendhon.e.c.m@gmail.com>
2021-02-11 15:46:37 -0300

    implement: Login and Logout

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -6,30 +6,39 @@ import { MEAT_API } from "../../app.api";
 import { User } from "./user.model";
 
 import "rxjs/add/operator/do";
-import { Router } from "@angular/router";
+import "rxjs/add/operator/filter";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Injectable()
 export class LoginService {
   user: User;
-  
+  lastUrl: string;
+
   constructor(
     private http: HttpClient,
     private route: Router,
-    ) {}
-  
+  ) {
+    this.route.events
+      .filter((e) => e instanceof NavigationEnd)
+      .subscribe((e: NavigationEnd) => this.lastUrl = e.url);
+  }
+
   isLoggedIn(): boolean {
     return this.user !== undefined;
   }
-  
+
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>(`${MEAT_API}/login`, {
       email: email,
       password: password,
     }).do((user) => this.user = user);
   }
-  
-  handleLogin(path?: string) {
-    this.route.navigate(['/login', btoa(path)]) // Transformando em Base 64 para a URL ser mais amigável
+
+  handleLogin(path: string = this.lastUrl) {
+    this.route.navigate(["/login", btoa(path)]); // Transformando em Base 64 para a URL ser mais amigável
   }
 
+  logout() {
+    this.user = undefined;
+  }
 }
