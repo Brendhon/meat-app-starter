ecf83ee0252784027ee8514e893853b2f09b27f4
Brendhon <brendhon.e.c.m@gmail.com>
2021-02-11 10:39:36 -0300

    implement: Authentication

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ import { Express } from "express";
 import * as fs from "fs";
 import * as https from "https";
 
+import { handleAuth } from "./auth";
+
 const server: Express = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -13,6 +15,8 @@ server.use(middlewares);
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
+
+server.post("/login", handleAuth);
 
 // Use default router
 server.use(router);
